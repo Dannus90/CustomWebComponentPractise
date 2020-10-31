c6e052af89073c8074b6ce5a44ad2465f52d112d
Daniel Persson <persson.daniel.1990@gmail.com>
2020-10-31 21:50:33 +0100

    Add callbacks

--- userCard.js
+++ userCard.js
@@ -14,7 +14,7 @@ template.innerHTML = `
 
         .user-card img {
             width: 100%;
-            height: 100%;
+            height: 170px;
         }
 
         .user-card button {
@@ -44,10 +44,37 @@ class UserCard extends HTMLElement {
   constructor() {
     super();
 
+    this.showInfo = true;
+
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
     this.shadowRoot.querySelector("img").src = this.getAttribute("avatar");
+  }
+
+  toggleInfo() {
+    this.showInfo = !this.showInfo;
+
+    const info = this.shadowRoot.querySelector(".info");
+    const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
+
+    if (this.showInfo) {
+      info.style.display = "block";
+      toggleBtn.innerText = "Hide Info";
+    } else {
+      info.style.display = "none";
+      toggleBtn.innerText = "Show Info";
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot
+      .querySelector("#toggle-info")
+      .addEventListener("click", () => this.toggleInfo());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector("#toggle-info").removeEventListener();
   }
 }
 
